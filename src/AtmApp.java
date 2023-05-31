d0e2e5eec725046749a0147f25b6720a43ec65cb
Hülya Vural <hulyaktas17@hotmail.com>
2023-06-01 01:55:14 +0300

    nsb1

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -8,6 +8,8 @@ public class AtmApp {
         //version3
         System.out.println("Javayı öğrenmek çok kolay");
         //version4
+        System.out.println("hülya branchi kodlarını yazdı.");
+        //nsb1
 
 
     }
